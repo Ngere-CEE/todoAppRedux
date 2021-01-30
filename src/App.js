c8df92211d2b90808c2a2c7c302c226cebb7ad81
Ngere-CEE <rogasngere@gmail.com>
2021-01-30 10:38:55 +0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import Dashboard from "./components/dashboard/Dashboard";
+import NavBar from "./components/layout/NavBar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <h3 className="text-primary">
-                  Todo App Redux
-        </h3>
-        <Route path="/signin" exact component={SignIn} />
-        <Route path="/signup" exact component={SignUp} />
-        <Route path="/" exact component={Dashboard} />
+        <NavBar />
+        <Switch>
+            <Route path="/signin" exact component={SignIn} />
+            <Route path="/signup" exact component={SignUp} />
+            <Route path="/" exact component={Dashboard} />
+        </Switch>
       </BrowserRouter>
       
     </>
---
+++ src/components/layout/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import NavItems from './NavItems'
+
+const NavBar = () =>{
+    return (
+        <>
+            <nav className="navbar navbar-dark bg-dark d-flex justify-content-center">
+                <Link to="/" className="navbar-brand">
+                    <h3>TodoApp</h3>
+                </Link>
+                <NavItems />
+
+            </nav>
+            
+        </>
+    )
+}
+
+export default NavBar

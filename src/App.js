edd7fde122716048b5cc29b1134075e232e87875
Ngere-CEE <rogasngere@gmail.com>
2021-01-30 10:23:35 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <>
-      <header>
-                 Learn React
-      </header>
+      <BrowserRouter>
+        <h3 className="text-primary">
+                  Todo App Redux
+        </h3>
+        <Route path="/signin" exact component={SignIn} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/" exact component={Dashboard} />
+      </BrowserRouter>
+      
     </>
   );
 }
---
+++ src/components/auth/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    render() {
+        return (
+            <>
+                <h3>SignUp</h3>
+            </>
+        );
+    }
+}
+
+export default SignUp;
